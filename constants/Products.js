9f4c8a8dfda8cfecab4a2568f6207fdafcbc6a28
Denis Yuriev <denis.yuriev@overteam.ru>
2019-05-01 18:24:47 +0300

    add const Product

---
+++ constants/Products.js
@@ -0,0 +1,22 @@
+const Products = [
+  { 
+    id: 1, 
+    title: 'Awesome product', 
+    price: '300', 
+    imageUrl: '/images/awesome_product.jpg' 
+  },
+  { 
+    id: 2, 
+    title: 'Not awesome product', 
+    price: '42', 
+    imageUrl: '/images/not_awesome_product.jpg' 
+  },
+  { 
+    id: 3, 
+    title: 'Weird product', 
+    price: '6', 
+    imageUrl: '/images/weird_product.jpg' 
+  }
+];
+
+export default Products;
